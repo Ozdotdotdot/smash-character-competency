063f429d07ec5dcb713b4833444ab5a21f473c21
Ozair Khan <ozairk11@gmail.com>
2025-11-08 23:59:36 -0500

    Ensuring the program works on different systems

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ozworkbook/smash-character-competency/.venv
+set -gx VIRTUAL_ENV /home/ozworkbook/code-repos/smashDA/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
